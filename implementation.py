116d6e3ae4ca7153197ffdf138ee4ddeb53834e2
Etzion Harari <etzionhar@gmail.com>
2020-09-14 15:46:43 +0300

    Update implementation.py

--- implementation.py
+++ implementation.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 
 # example 1
-filename = 'example_greenwich.csv'
+filename = 'Examples\example_greenwich.csv'
 data = pd.read_csv(filename)
-create_sector_kml(data,'x','y','angle','dis','sd',name='name',output='POLYGONS_GREENWICH')
+create_sector_kml(data,'x','y','angle','dis','sd',name='name',output='Outputs\POLYGONS_GREENWICH')
 
 
 # example 2
-filename = 'example_new_york_harbor.csv'
+filename = 'Examples\example_new_york_harbor.csv'
 data = pd.read_csv(filename)
 name = 'name'
 x_field = 'x'
@@ -17,16 +17,16 @@ y_field = 'y'
 angle = 'angle'
 distance = 'distance'
 std = 'sd'
-create_sector_kml(data,x_field,y_field,angle,distance,std,name='name',output='POLYGONS_NYH')
+create_sector_kml(data,x_field,y_field,angle,distance,std,name='name',output='Outputs\POLYGONS_NYH')
 
 
 # example 3
-filename = 'example_paris.csv'
+filename = 'Examples\example_paris.csv'
 data = pd.read_csv(filename)
-create_sector_kml(data,'x_point','y_point','degree','dis','std',name='nickname',output='POLYGONS_PARIS')
+create_sector_kml(data,'x_point','y_point','degree','dis','std',name='nickname',output='Outputs\POLYGONS_PARIS')
 
 
 # example 4
-filename = 'example_Sydney.csv'
+filename = 'Examples\example_Sydney.csv'
 data = pd.read_csv(filename)
-create_sector_kml(data,'X','Y','degre','dist','sd',name='place',output='POLYGONS_SYDNEY')
+create_sector_kml(data,'X','Y','degre','dist','sd',name='place',output='Outputs\POLYGONS_SYDNEY')
